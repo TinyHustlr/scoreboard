3539c2c962b68fb3f5914586afe5a3f494a990ac
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-08-20 12:59:48 +1000

    Foul counter working

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let home = document.getElementById("home_score")
 let guest = document.getElementById("guest_score")
+let homeF = document.getElementById("home_foul")
+let guestF = document.getElementById("guest_foul")
+
 let homeScore = 0
 let guestScore = 0
+let homeFoul = 0
+let guestFoul = 0
 
 // Diplay initial scores as zero
 home.textContent = homeScore
 guest.textContent = guestScore
+homeF.textContent = homeFoul
+guestF.textContent = guestFoul
 
 // Initial Text color
 home.style.color = "rgb(246, 1, 157)"
@@ -105,11 +112,26 @@ function guest3() {
     }
 }
 
+// Foul scoring
+function homefoul() {
+    homeFoul += 1
+    homeF.textContent = homeFoul
+}
+
+function guestfoul() {
+    guestFoul += 1
+    guestF.textContent = guestFoul
+}
+
 function newgame() {
     home.textContent = 0
     guest.textContent = 0
+    homeF.textContent = 0
+    guestF.textContent = 0
     homeScore = 0
     guestScore = 0
+    homeFoul = 0
+    guestFoul = 0
     home.style.color = "rgb(246, 1, 157)"
     guest.style.color = "rgb(246, 1, 157)"
 }
